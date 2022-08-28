fc9dece7dc5cc5c7dea7119832310ebc217bcc36
B0user <56859914+B0user@users.noreply.github.com>
2022-08-28 10:33:52 +0600

    Configuration for deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const allowedOrigins = [
     `http://localhost:${process.env.PORT}`,
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://inroom.tech',
+    'http://inroom.tech:2000',
+    'http://inroom.tech:3000'    
 ];
 
 module.exports = allowedOrigins;
--- controllers/api/uploadController.js
+++ controllers/api/uploadController.js
@@ -9,25 +9,27 @@ const uploadFiles = async (req, res) => {
                 message: 'No file info uploaded'
             });
         } else {
-            const image = req.files.image;
+            // const image = req.files.image;
             const model = req.files.model;
             
-            const image_path = path.join('.', 'public', 'media', 'images', client_id, product_id, image.name);
-            const model_path = path.join('.', 'public', 'media', 'models', client_id, product_id, model.name);
+            // const image_path = path.join('.', 'public', 'media', 'images', client_id, product_id, image?.name);
+            const model_path = path.join('.', 'public', 'media', 'models', client_id, product_id, model?.name);
             
             // Production
             if (process.env.NODE_ENV === 'production') {
-                image_path = path.join('.', 'public', 'media', 'images', client_id, product_id, image.name);
-                model_path = path.join('.', 'public', 'media', 'models', client_id, product_id, model.name);
+                // image_path = path.join('.', 'public', 'media', 'images', client_id, product_id, image?.name);
+                model_path = path.join('.', 'public', 'media', 'models', client_id, product_id, model?.name);
             }
 
             //Use the mv() method to place the file in directory
-            image?.mv(image_path);
+            // image?.mv(image_path);
             model?.mv(model_path);
             //send response
             res.sendStatus(201);
+            res.json({ model_path});
         }
     } catch (err) {
+        console.error(err);
         res.status(500).send(err);
     }
 }
