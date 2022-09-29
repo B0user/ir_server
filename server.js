63d12ec4e61987fe2f86fa8ceb49375e1866b9c6
B0user <56859914+B0user@users.noreply.github.com>
2022-09-29 19:50:57 +0600

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true, limit: '10mb'}));
 
 // built-in middleware for json 
-app.use(express.json({limit: '1kb'}));
+app.use(express.json({limit: '10mb'}));
 
 // HPP
 app.use(hpp());
