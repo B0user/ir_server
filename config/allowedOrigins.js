08f112c7f26f6bc9593d0b75aede7afdad720fba
B0user <site88sonic.ammm@gmail.com>
2023-03-05 20:06:14 +0600

    add local network

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -8,7 +8,8 @@ const allowedOrigins = [
     'http://localhost:2000',
     'http://127.0.0.1:3000',
     'http://37.99.123.117:3000',
-    'http://5.34.113.45:3000'
+    'http://5.34.113.45:3000',
+    'http://192.168.0.111:3000'
 ];
 
 module.exports = allowedOrigins;
